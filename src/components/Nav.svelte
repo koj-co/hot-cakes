7edab16eab6d48077f1f395c2b653a61484d4860
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-11 14:08:17 +0530

    :lipstick: Add navbar with dark theme

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -15,29 +15,44 @@
     SideNavMenuItem,
     SkipToContent,
   } from "carbon-components-svelte";
-  import SettingsAdjust20 from "carbon-icons-svelte/lib/SettingsAdjust20";
+  import Moon20 from "carbon-icons-svelte/lib/Moon20";
 
   let isSideNavOpen = false;
   let isOpen = false;
+  let dark = undefined;
+  let href = "";
+
+  $: {
+    href = dark
+      ? "https://unpkg.com/carbon-components-svelte@0.22.0/css/g100.css"
+      : "https://unpkg.com/carbon-components-svelte@0.22.0/css/g10.css";
+  }
 </script>
 
+<style>
+  :global(.OK .bx--toggle-input__label .bx--toggle__switch) {
+    margin-top: 0;
+  }
+</style>
+
+<svelte:head>
+  <link rel="stylesheet" {href} />
+</svelte:head>
 <Header company="🍰 Koj" platformName="Hot Cakes" bind:isSideNavOpen>
   <div slot="skip-to-content">
     <SkipToContent />
   </div>
   <HeaderUtilities>
     <HeaderActionSearch />
-    <HeaderGlobalAction aria-label="Settings" icon={SettingsAdjust20} />
+    <HeaderGlobalAction
+      aria-label="Toggle dark mode"
+      icon={Moon20}
+      on:click={() => (dark = !dark)} />
     <HeaderAction bind:isOpen>
       <HeaderPanelLinks>
-        <HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
-        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
-        <HeaderPanelDivider>Switcher subject 2</HeaderPanelDivider>
-        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
-        <HeaderPanelLink>Switcher item 2</HeaderPanelLink>
-        <HeaderPanelLink>Switcher item 3</HeaderPanelLink>
-        <HeaderPanelLink>Switcher item 4</HeaderPanelLink>
-        <HeaderPanelLink>Switcher item 5</HeaderPanelLink>
+        <HeaderPanelDivider>Admin</HeaderPanelDivider>
+        <HeaderPanelLink>Change password</HeaderPanelLink>
+        <HeaderPanelLink>Logout</HeaderPanelLink>
       </HeaderPanelLinks>
     </HeaderAction>
   </HeaderUtilities>
